cc5420537f56426bb606613bb0b7683439c15c56
Glenn Harper <glenn@Glenns-MacBook-Pro.local>
2023-12-16 14:58:34 -0600

    init

---
+++ afternoon_projects/Connect4.py
@@ -0,0 +1,47 @@
+# # Connect4
+# Board Representation:
+
+# Think about how you want to represent the Connect Four board. You could use a list of lists or a 2D array.
+# Displaying the Board:
+
+# Write a function to display the current state of the board. This could be a simple text-based representation.
+# Player Input:
+
+# Implement a function to get input from players. Make sure to validate the input (e.g., ensure the column is not full, accept only valid column numbers).
+# Making Moves:
+
+# Write a function to update the board based on the player's move.
+# Check for a Win:
+
+# Implement a function to check if a player has won the game. This involves checking for four consecutive discs in a row, column, or diagonal.
+# Switching Players:
+
+# Write a mechanism to switch between players after each move.
+# Main Game Loop:
+
+# Create a loop to keep the game running until there's a winner or the board is full (resulting in a draw).
+# End of Game:
+
+# Display the winner or announce a draw at the end of the game.
+
+grid = [ 
+    # 7-column, 6-row
+    ['X' for i in range(7)] for j in range(6)
+]
+
+def render_grid():
+    pass
+
+def handle_player_input():
+    pass
+
+
+
+
+
+
+
+
+
+
+
