805b71cb3e3c240c2cb37b03157fe14ee71f4042
Glenn Harper <glenn@Glenns-MacBook-Pro.local>
2023-12-16 18:34:37 -0600

    ss

--- afternoon_projects/Connect4.py
+++ afternoon_projects/Connect4.py
@@ -1,11 +1,7 @@
+import numbers
 # # Connect4
 # Board Representation:
 
-# Think about how you want to represent the Connect Four board. You could use a list of lists or a 2D array.
-# Displaying the Board:
-
-# Write a function to display the current state of the board. This could be a simple text-based representation.
-# Player Input:
 
 # Implement a function to get input from players. Make sure to validate the input (e.g., ensure the column is not full, accept only valid column numbers).
 # Making Moves:
@@ -23,21 +19,130 @@
 # End of Game:
 
 # Display the winner or announce a draw at the end of the game.
+FILLED_CELL = '[0]'
+EMPTY_CELL = '[ ]'
 
+grid_columns = 7
+grid_rows = 6
 grid = [ 
     # 7-column, 6-row
-    ['X' for i in range(7)] for j in range(6)
+    [EMPTY_CELL for i in range(grid_columns)] for j in range(grid_rows)
 ]
+current_player = 0
+total_players = 2
+
+
+def render_ui():
+    global current_player
+    print(f"Player {current_player}'s turn...\n... GO!")
 
 def render_grid():
-    pass
+    delimiter = " | "
+    render_string = ""
+    
+    # Contstruct render string
+    for row in grid:
+        render_string += delimiter
+        for cell in row:
+            render_string += cell + delimiter
+        render_string += '\n'
+    
+    # Print string
+    print(render_string)
 
 def handle_player_input():
+    input_validated = False
+
+    # Get input
+    while(not input_validated):
+        column_selection = -1
+        row_selection = -1
+
+        # Get column selection
+        user_input = input("Enter the column of your selected cell: ")
+        try:    
+            # Validate column selection
+            assert isinstance(user_input, numbers.Real)
+            user_input = int(user_input)
+            assert user_input <= grid_columns
+
+            column_selection = user_input-1
+        except Exception as e: 
+            print("Unacceptable. Please enter a valid column number.\n"+ e)
+            continue
+
+        # Get row selection
+        user_input = input("Enter the row of your selected cell: ")
+        try:    
+            # Validate row selection
+            assert isinstance(user_input, numbers.Real)
+            user_input = int(user_input)
+            assert user_input <= grid_rows
+
+            row_selection = user_input-1
+        except Exception as e: 
+            print("Unacceptable. Please enter a valid row number.\n"+ e)
+            continue
+            
+        print(f"Selection ({column_selection+1},{row_selection+1}) confirmed.")
+        input_validated=True
+    
+    # Update grid
+    grid[column_selection][row_selection] = FILLED_CELL
+
+def update_grid():
     pass
 
+def check_game_over():
+    # Check if a player has won or if the board is full
+    pass
 
+def end_game():
+    end_string = "STOP!!\n" + 
+                "... \n" +
+                f"Player {current_player} WINS!!!"
+    print(end_string)
 
+def main():
+    # global current_player
+    run_flag = True
 
+    # Intro text
+    intro_text = "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n" +
+                "Connect 4 : Connect 4 to win!!\n" +
+                "+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=\n"
+    print(intro_text)
+
+    # Initialize Game
+    current_player = 1
+
+    # Game loop
+    while(run_flag):
+        # Render UI
+        render_ui()
+
+        # Render grid
+        render_grid()
+
+        # Handle input
+        response = handle_player_input()
+        if response == "QUIT":
+            print("Game Over... \nNo winner decided.")
+            break
+
+        # Switch Players
+        current_player += 1
+        if current_player > total_players:
+            current_player = 1
+
+        # Check if game is over
+        game_over = check_winner_exists()
+        if game_over: 
+            end_game()
+            break
+
+if __name__=='__main__':
+    main()
 
 
 
